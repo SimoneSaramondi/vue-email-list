dcd4e0552803d6b97d197f79197592ba07082f10
SimoneSaramondi <simo98sara@gmail.com>
2021-05-11 15:33:13 +0200

    print num mails

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const app = new Vue({
     el:'#app',
     data:{
+        randomMail: []
+    },
+    mounted(){
+
+        const self = this;
+
+        for(let i = 0; i < 10; i++){
+
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then(function (risposta){
+            self.randomMail.push(risposta.data.response)
+
+            });
+        }
         
-    }
+    },
 })
